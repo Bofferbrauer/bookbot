eec4973a819fbffe039f6a44a059183eacea757d
Bofferbrauer <halwen_chemiker@hotmail.com>
2025-06-10 13:10:11 +0200

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+def get_book_text():
+	with open("/books/frankenstein.txt") as f:
+		file_contents = f.read()
+		print(file_contents)
+
+def main(get_book_text):
+	print(get_book_text)
+	return main()
